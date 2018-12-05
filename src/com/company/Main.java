77d1093076e2ac644174a51149eb1f3276f9d175
zhangmingyang <1154540719@qq.com>
2018-12-05 14:52:25 +0800

    000

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("刘狗");
-        System.out.println("陶狗");
+        System.out.println("狗狗");
     }
 }
